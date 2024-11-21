793ca825a9e531052c08b612e4db24eb81571c60
cx20012002 <cx20012002@gmail.com>
2024-11-22 02:49:24 +1300

    commit 4

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,14 @@
+"use client";
+
 import React from 'react'
+import { useBearStore } from '../(store)/store'
 
 export default function About() {
+  const {bears, increase} = useBearStore();
   return (
     <div>
-      <h1>About</h1>
+      <h2>Bears: {bears}</h2>
+      <button className='bg-primary p-2 rounded-md texthw' onClick={() => increase(1)}>Increase Bears</button>
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat, praesentium unde. Ducimus nemo debitis cupiditate natus architecto pariatur perspiciatis commodi! Distinctio cupiditate qui vitae modi doloribus suscipit dolorem sequi soluta iusto velit? Doloribus dolor animi architecto tempore earum harum, possimus quibusdam voluptatibus ut qui quaerat temporibus repellendus expedita excepturi. Mollitia.</p>
     </div>
   )
